f22c2a72a1486702fb17476d56e780035dbb459a
Chaise Hocking <chaise.hocking@shinetech.com>
2016-03-18 21:41:18 +1100

    Initial Commit

---
+++ DelegateCrashDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DelegateCrashDemo
+//
+//  Created by Chaise Hocking on 18/03/2016.
+//  Copyright © 2016 Shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
